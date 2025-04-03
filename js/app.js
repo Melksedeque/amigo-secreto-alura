dc76b9f5a5c706c6c00586e7de885199f7277f2e
Melksedeque <freelancer@melksedeque.com.br>
2025-04-03 15:55:09 -0300

    Criação da função embaralha

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
+let amigos = [];
+
 function adicionar() {
   let amigo = document.getElementById("nome-amigo").value;
   let lista = document.getElementById("lista-amigos");
 
+  amigos.push(amigo);
   if (lista.textContent == "") {
     lista.textContent = amigo;
   } else {
     lista.textContent = lista.textContent + ", " + amigo;
+  }
+
+  amigo = "";
+}
+
+function sortear() {
+  embaralha(amigos);
+}
+
+function embaralha(lista) {
+  for (let indice = lista.length; indice; indice--) {
+    const indiceAleatorio = Math.floor(Math.random() * indice);
+
+    // atribuição via destructuring
+    [lista[indice - 1], lista[indiceAleatorio]] = [
+      lista[indiceAleatorio],
+      lista[indice - 1],
+    ];
   }
 }
